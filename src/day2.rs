6d2f7fca9bbc0bf543a7f6ab4eabca4dcb8212e6
Md Faizaan <aulisius7@gmail.com>
2016-12-02 20:21:57 +0530

    Day 2 improvements

--- src/day2.rs
+++ src/day2.rs
@@ -1,72 +1,42 @@
-fn is_legal_p1(i: i32, c: i32) -> bool {
-    let moves = match i {
-        1 | 3 => vec![2, i + 3],
-        2 | 8 => vec![i - 1, i + 1, 5],
-        4 | 6 => vec![i - 3, 5, i + 3],
-        5 => vec![2, 4, 6, 8],
-        7 | 9 => vec![i - 3, 8],
-        _ => vec![0], 
-    };
-    moves.contains(&(c + i))
+fn next_p1(i: i32, c: usize) -> i32 {
+  let moves = match i {
+    1 => [1, 1, 2, 4],
+    2 | 3 => [i - 1, i, 3, i + 3],
+    4 | 5 => [4, i - 3, i + 1, i + 3],
+    6 | 9 => [i - 1, i - 3, i, 9],
+    7 | 8 => [7, i - 3, i + 1, i],
+    _ => [0, 0, 0, 0]
+  };
+  moves[c]
 }
 
-fn is_legal_p2(i: i32, c: i32) -> bool {
-    let moves = match i {
-        1 => vec![3],
-        2 | 4 => vec![3, i + 4],
-        3 => vec![1, 2, 4, 7],
-        5 => vec![6],
-        6 | 7 | 8 => vec![i - 4, i - 1, i + 1, i + 4],
-        9 => vec![8],
-        10 | 12 => vec![i - 4, 11],
-        11 => vec![7, 10, 12, 13],
-        13 => vec![11],
-        _ => vec![0],
-    };
-    moves.contains(&(c + i))
+fn next_p2(i: i32, c: usize) -> i32 {
+  let moves = match i {
+    1 => [1, 1, 1, 3],
+    2 => [2, 2, 3, 6],
+    3 => [2, 1, 4, 7],
+    4 => [3, 4, 4, 8],
+    5 => [5, 5, 6, 5],
+    6 | 7 | 8 => [i - 1, i - 4, i + 1, i + 4],
+    9 => [8, 9, 9, 9],
+    10 => [10, 6, 11, 10],
+    11 => [10, 7, 12, 13],
+    12 => [11, 8, 12, 12],
+    13 => [13, 11, 13, 13],
+    _ => [0, 0, 0, 0]
+  };
+  moves[c]
 }
 
-fn get_value_p1(i: char) -> i32 {
-    match i {
-        'D' => 3,
-        'U' => -3,
-        'L' => -1,
-        'R' => 1,
-        _ => 0,
-    }
-}
-
-fn get_value_p2(i: char, c: i32) -> i32 {
-    match i {
-        'D' if c == 1 || c == 11 => 2,
-        'U' if c == 3 || c == 13 => -2,
-        'D' => 4,
-        'U' => -4,
-        'L' => -1,
-        'R' => 1,
-        _ => 0,
-    }
-}
+fn get_value(i: char) -> usize {match i {'L' => 0, 'U' => 1, 'R' => 2, 'D' => 3, _ => 0}}
 
 pub fn bathroom_code() {
-    let input = include_str!("d2");
-
-    let mut a = 5;
-    let mut b = 5;
-    let codes: Vec<_> = input.lines()
-        .map(|line| {
-            a = line.chars()
-                .map(get_value_p1)
-                .fold(a, |acc, x| if is_legal_p1(acc, x) { acc + x } else { acc });
-            b = line.chars().fold(b, |acc, e| {
-                let x = get_value_p2(e, acc);
-                if is_legal_p2(acc, x) { acc + x } else { acc }
-            });
-            (a, b)
-        })
-        .collect();
-
-    let (p1, p2): (Vec<_>, Vec<_>) = codes.iter().cloned().unzip();
-
-    println!("Answers \nPart 1: {:?} \nPart 2: {:?}", p1, p2);
+  let (p1, p2): (Vec<_>, Vec<_>) = include_str!("d2").lines()
+    .map(|line| line.chars()
+         .zip(line.chars())
+         .map(|(x, y)| (get_value(x), get_value(y)))
+         .fold((5, 5), |(a, b), (x, y)| (next_p1(a, x), next_p2(b, y)))
+        )
+    .unzip();
+  println!("Answers\nPart 1: {:?}\nPart 2: {:?}", p1, p2);
 }
